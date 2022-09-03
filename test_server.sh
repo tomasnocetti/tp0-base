3301ce78840df8a05b67ee314ee5b1207778379d
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-09-03 15:43:32 -0300

    feat: add bash script for testing server

---
+++ test_server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# set -v
+
+SERVICE_NAME="${1:-server}"
+PORT="${2:-12345}"
+STRING_TEST='this is a great server'
+
+### COLORS
+GREEN='\033[0;32m'
+BIGreen='\033[1;92m'
+BIReed='\033[1;91m'
+NC='\033[0m' # No Color
+
+echo "${GREEN}## BUILDING IMAGE ## ${NC}"
+
+docker build ./netcat -t netcat-test
+
+echo "${GREEN}## TESTING SERVER ## ${NC}"
+var=$(echo "${STRING_TEST}" | \
+    docker run -i --rm --network=testing_net netcat-test nc ${SERVICE_NAME} ${PORT})
+
+if [[ "$var" == *"$STRING_TEST"* ]]; then
+  echo "${BIGreen}Congrats! You'r server it's there."
+else
+  echo "${BIReed}Ups! No correct response received. ${var}"
+fi
