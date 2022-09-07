8dc22ca33b054642452e1b487c3b54a5d606b7f6
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-09-07 20:18:45 -0300

    feat: add persistance

--- server/common/persistance.py
+++ server/common/persistance.py
@@ -1,4 +1,4 @@
-from multiprocessing import Lock
+from multiprocessing import Lock, Value
 from typing import List
 
 from common.utils import Contestant, persist_winners
@@ -7,6 +7,8 @@ from common.utils import Contestant, persist_winners
 class Persistance:
     def __init__(self) -> None:
         self.lock = Lock()
+        self.total_winners = Value('i', 0)
+        self.total_agencies_pending = Value('i', 0)
 
     def persist_winners(self, con: List[Contestant]):
         self.lock.acquire()
@@ -14,3 +16,12 @@ class Persistance:
         persist_winners(con)
 
         self.lock.release()
+
+    def increment_agencies_pending(self):
+        self.total_agencies_pending.value += 1
+
+    def decrement_agencies_pending(self):
+        self.total_agencies_pending.value -= 1
+
+    def add_winners_to_stats(self, size):
+        self.total_winners.value += size
